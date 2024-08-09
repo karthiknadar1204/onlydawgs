f6108708d001446fb633c5db42e1e0a9e312bc61
karthik <karthiknadar1204@gmail.com>
2024-08-09 11:45:34 +0530

    added merchandise components to the right

--- src/components/SuggestedProducts.tsx
+++ src/components/SuggestedProducts.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import SuggestedProduct from './SuggestedProduct'
+import { products } from '@/dummy_data'
 
 const SuggestedProducts = () => {
   return (
-    <div>
-      <SuggestedProduct/>
-      <SuggestedProduct/>
-      <SuggestedProduct/>
-      <SuggestedProduct/>
-      <SuggestedProduct/>
-      <SuggestedProduct/>
+    <div className='lg:w-2/5 hidden lg:flex flex-col gap-3 px-2 sticky top-0 right-0 h-screen ml-3'>
+      <div className='flex flex-col gap-2 mt-20' >
+      <p className='uppercase text-muted-foreground font-semibold tracking-tight'>Recommended Products</p>
+      <div className='grid grid-cols-2 gap-4' >
+      {products.map((product) => (
+						<SuggestedProduct key={product.id} product={product} />
+					))}
+      </div>
+
+      </div>
     </div>
   )
 }
