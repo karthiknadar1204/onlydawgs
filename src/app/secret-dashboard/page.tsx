5e3714616cd1780a4e14129a87adb09937e2d0e2
karthik <karthiknadar1204@gmail.com>
2024-08-09 12:26:51 +0530

    added secret dashboard page

---
+++ src/app/secret-dashboard/page.tsx
@@ -0,0 +1,10 @@
+import BaseLayout from '@/components/BaseLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <BaseLayout renderRightPanel={false}>page</BaseLayout>
+  )
+}
+
+export default page
