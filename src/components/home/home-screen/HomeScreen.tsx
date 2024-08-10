51d7bcca1a636239dc5b7941754e700ad6e85dcd
karthik <karthiknadar1204@gmail.com>
2024-08-10 11:34:38 +0530

    likes and comment feature added

--- src/components/home/home-screen/HomeScreen.tsx
+++ src/components/home/home-screen/HomeScreen.tsx
@@ -6,16 +6,18 @@ import { getUserProfileAction } from "@/app/update-profile/actions";
 import { notFound } from "next/navigation";
 
 const HomeScreen = async () => {
-	const admin = await prisma.user.findUnique({ where: { email: process.env.ADMIN_EMAIL } });
-	const user = await getUserProfileAction();
+  const admin = await prisma.user.findUnique({
+    where: { email: process.env.ADMIN_EMAIL },
+  });
+  const user = await getUserProfileAction();
 
-	if (!user) return notFound();
+  if (!user) return notFound();
 
-	return (
-		<BaseLayout>
-			<UserProfile />
-			<Posts admin={admin!} isSubscribed={user?.isSubscribed} />
-		</BaseLayout>
-	);
+  return (
+    <BaseLayout>
+      <UserProfile />
+      <Posts admin={admin!} isSubscribed={user?.isSubscribed} />
+    </BaseLayout>
+  );
 };
 export default HomeScreen;
