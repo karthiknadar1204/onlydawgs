c8886e2d050fd85de8669d1991691563278a864d
karthik <karthiknadar1204@gmail.com>
2024-08-10 09:45:04 +0530

    products display

--- src/app/merch/[id]/page.tsx
+++ src/app/merch/[id]/page.tsx
@@ -1,66 +1,42 @@
-// import BaseLayout from '@/components/BaseLayout'
-// import React from 'react'
-// import ProductCheckout from './ProductCheckout'
-// import UnderlinedText from '@/components/decorators/UnderlinedText'
-// import ProductCard from '@/components/ProductCard'
-// import { products } from '@/dummy_data'
-
-// const page = () => {
-//   return (
-//     <BaseLayout renderRightPanel={false}>
-//       <div className='px-3 md:px-7 my-20'>
-//         <ProductCheckout/>
-//         <h1 className='text-3xl text-center mt-20 mb-10 font-bold tracking-tight'>
-// 					More product from{" "}
-// 					<UnderlinedText className='decoration-wavy underline-offset-8'>OnlyHorse</UnderlinedText>
-// 				</h1>
-//         <div className='grid gap-5 grid-cols-1 md:grid-cols-2'>
-// 					{products.map((product) => (
-// 						<ProductCard key={product.id} product={product} />
-// 					))}
-// 				</div>
-
-//       </div>
-//     </BaseLayout>
-//   )
-// }
-
-// export default page
-
-
-
-
-
 import BaseLayout from "@/components/BaseLayout";
-import React from "react";
-import ProductCheckout from "./ProductCheckout";
-import UnderlinedText from "@/components/decorators/UnderlinedText";
 import ProductCard from "@/components/ProductCard";
-import { products } from "@/dummy_data";
+import UnderlinedText from "@/components/decorators/UnderlinedText";
+import ProductCheckout from "./ProductCheckout";
+import prisma from "@/db/prisma";
+import { notFound } from "next/navigation";
 
-const page = () => {
-  const mainProduct = products[0]; // Assuming the first product is the main one to display
+const Page = async ({ params }: { params: { id: string } }) => {
+	const currentProduct = await prisma.product.findUnique({
+		where: {
+			id: params.id,
+		},
+	});
 
-  return (
-    <BaseLayout renderRightPanel={false}>
-      <div className="px-3 md:px-7 my-20">
-        {/* Pass the selected product to ProductCheckout */}
-        <ProductCheckout product={mainProduct} />
+	const products = await prisma.product.findMany({
+		where: {
+			isArchived: false,
+			id: { not: params.id },
+		},
+	});
 
-        <h1 className="text-3xl text-center mt-20 mb-10 font-bold tracking-tight">
-          More products from{" "}
-          <UnderlinedText className="decoration-wavy underline-offset-8">
-            OnlyDawgs
-          </UnderlinedText>
-        </h1>
-        <div className="grid gap-5 grid-cols-1 md:grid-cols-2">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
-    </BaseLayout>
-  );
+	if (!currentProduct || currentProduct.isArchived) return notFound();
+
+	return (
+		<BaseLayout renderRightPanel={false}>
+			<div className='px-3 md:px-7 my-20'>
+				<ProductCheckout product={currentProduct} />
+
+				<h1 className='text-3xl text-center mt-20 mb-10 font-bold tracking-tight'>
+					More product from{" "}
+					<UnderlinedText className='decoration-wavy underline-offset-8'>OnlyDawgs</UnderlinedText>
+				</h1>
+				<div className='grid gap-5 grid-cols-1 md:grid-cols-2'>
+					{products.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))}
+				</div>
+			</div>
+		</BaseLayout>
+	);
 };
-
-export default page;
+export default Page;
--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -1,25 +1,29 @@
 import BaseLayout from "@/components/BaseLayout";
-import UnderlinedText from "@/components/decorators/UnderlinedText";
 import ProductCard from "@/components/ProductCard";
-import { products } from "@/dummy_data";
-import React from "react";
+import UnderlinedText from "@/components/decorators/UnderlinedText";
+import prisma from "@/db/prisma";
 
-const page = () => {
-  return (
-    <BaseLayout renderRightPanel={false}>
-      <div className="px-3 md:px-10 my-10">
-        <h1 className="text-3xl text-center my-5 font-bold tracking-tight">
-          Our{" "}
-          <UnderlinedText className="decoration-wavy">Products</UnderlinedText>
-        </h1>
-        <div className="grid gap-5 grid-cols-1 md:grid-cols-2">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
-    </BaseLayout>
-  );
+const Page = async () => {
+	const products = await prisma.product.findMany({
+		where: {
+			isArchived: false,
+		},
+	});
+
+	return (
+		<BaseLayout renderRightPanel={false}>
+			<div className='px-3 md:px-10 my-10'>
+				<h1 className='text-3xl text-center my-5 font-bold tracking-tight'>
+					Our <UnderlinedText className='decoration-wavy'>Products</UnderlinedText>
+				</h1>
+
+				<div className='grid gap-5 grid-cols-1 md:grid-cols-2'>
+					{products.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))}
+				</div>
+			</div>
+		</BaseLayout>
+	);
 };
-
-export default page;
+export default Page;
